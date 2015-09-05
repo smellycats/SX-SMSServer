0a7738375f7637b08e30b971547ca59dede3b478
SmellyCat <smellycat2014@foxmail.com>
2015-09-06 03:39:22 +0800

    Add sms webservice config

--- ini_conf.py
+++ ini_conf.py
@@ -5,46 +5,36 @@ class MyIni:
     def __init__(self, confpath = 'my_ini.conf'):
         self.cf = ConfigParser.ConfigParser()
         self.cf.read(confpath)
-    
-    def get_kakou(self):
-        conf = {}
-        conf['host']     = self.cf.get('KAKOU', 'host')
-        conf['port']     = self.cf.getint('KAKOU', 'port')
-        conf['user']     = self.cf.get('KAKOU', 'user')
-        conf['password'] = self.cf.get('KAKOU', 'password')
-        return conf
-    
-    def get_hbc(self):
-        conf = {}
-        conf['host']     = self.cf.get('HBC', 'host')
-        conf['port']     = self.cf.getint('HBC', 'port')
-        conf['user']     = self.cf.get('HBC', 'user')
-        conf['password'] = self.cf.get('HBC', 'password')
-        return conf
 
     def get_mysql(self):
         conf = {}
-        conf['host']     = self.cf.get('MYSQLSET','host')
-        conf['port']     = self.cf.getint('MYSQLSET','port')
-        conf['user']     = self.cf.get('MYSQLSET','user')
-        conf['password'] = self.cf.get('MYSQLSET','password')
-        conf['db']       = self.cf.get('MYSQLSET','db')
+        conf['host'] = self.cf.get('MYSQL', 'host')
+        conf['port'] = self.cf.getint('MYSQL', 'port')
+        conf['user'] = self.cf.get('MYSQL', 'user')
+        conf['pwd']  = self.cf.get('MYSQL', 'pwd')
+        conf['db']   = self.cf.get('MYSQL', 'db')
         return conf
 
-    def get_syst(self):
+    def get_sys(self):
         conf = {}
-        conf['id_flag'] = self.cf.getint('SYSTSET','id_flag')
-        conf['imgpath'] = self.cf.get('SYSTSET','imgpath')
+        conf['secret_key'] = self.cf.get('SYS', 'secret_key')
+        conf['host']       = self.cf.get('SYS', 'host')
+        conf['port']       = self.cf.getint('SYS', 'port')
+        conf['while_list_open'] = self.cf.getboolean('SYS', 'while_list_open')
+        conf['while_list']      = self.cf.get('SYS', 'while_list')
         return conf
 
- 
-if __name__ == "__main__":
-    try:
-        hbcini = Hbc_ini()
-        print hbcini.getHzkk()
-        #s = imgIni.getPlateInfo(PATH2)
+    def get_webservice(self):
+        conf = {}
+        conf['url']     = self.cf.get('WEBSERVICE', 'url')
+        conf['db_ip']   = self.cf.get('WEBSERVICE', 'db_ip')
+        conf['db_name'] = self.cf.get('WEBSERVICE', 'db_name')
+        conf['db_port'] = self.cf.get('WEBSERVICE', 'db_port')
+        conf['user']    = self.cf.get('WEBSERVICE', 'user')
+        conf['pwd']     = self.cf.get('WEBSERVICE', 'pwd')
+        return conf
 
-    except ConfigParser.NoOptionError,e:
-        print e
-        time.sleep(10)
+    def __del__(self):
+        del self.cf
+
 
