ee2f7d38fdfa7d33fe9f72fb1738f8d7ef745226
SmellyCat <smellycat2014@foxmail.com>
2015-09-13 02:58:14 +0800

    Fix bug run.py

--- run.py
+++ run.py
@@ -3,22 +3,17 @@ from sms import debug_logging, online_logging, access_logging
 from ini_conf import MyIni
 
 if __name__ == '__main__':
-    debug_logging(u'logs\\error.log')
-    access_logging(u'logs\\access.log')
+    debug_logging(u'logs/error.log')
+    access_logging(u'logs/access.log')
 
     my_ini = MyIni()
     mysql_ini = my_ini.get_mysql()
     sys_ini = my_ini.get_sys()
-    #web_ini = my_ini.get_webservice()
 
     app.config['SECRET_KEY'] = sys_ini['secret_key']
-    app.config['HOST']       = sys_ini['host']
-    #app.config['PORT']       = sys_ini['port']
     app.config['WHITE_LIST_OPEN'] = sys_ini['while_list_open']
-    wl = sys_ini['while_list'].split(',')
-    if wl != ['']:
-        app.config['WHITE_LIST'] = set(wl) | app.config['WHITE_LIST']
+    app.config['WHITE_LIST'] = sys_ini['while_list'].split(',')
     app.config['SQLALCHEMY_BINDS']['sms'] = 'mysql://%s:%s@%s:%s/%s' % (mysql_ini['user'], mysql_ini['pwd'], mysql_ini['host'], str(mysql_ini['port']), mysql_ini['db'])
     app.config['SMS_WSDL_PARAMS'] = my_ini.get_webservice()
-    #print app.config
-    app.run(port=sys_ini['port'], threaded=True)
+    del my_ini
+    app.run(host=sys_ini['host'], port=sys_ini['port'], threaded=True)
