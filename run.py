f827d133605f2eb08a5572e63f453b275cc91a34
SmellyCat <smellycat2014@foxmail.com>
2015-10-21 23:30:09 +0800

    Fix bug run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from sms import app, views
+from sms import app
 
 if __name__ == '__main__':
     app.run()
--- run_test.py
+++ run_test.py
@@ -1,4 +1,4 @@
-from sms import app, views
+from sms import app
 from ini_conf import MyIni
 
 if __name__ == '__main__':
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     app.config['SECRET_KEY'] = sys_ini['secret_key']
     app.config['WHITE_LIST_OPEN'] = sys_ini['while_list_open']
     app.config['WHITE_LIST'] = sys_ini['while_list'].split(',')
-    app.config['SQLALCHEMY_BINDS']['sms'] = 'mysql://%s:%s@%s:%s/%s' % (mysql_ini['user'], mysql_ini['pwd'], mysql_ini['host'], str(mysql_ini['port']), mysql_ini['db'])
+    app.config['SQLALCHEMY_DATABASE_URI']= 'mysql://%s:%s@%s:%s/%s' % (
+        mysql_ini['user'], mysql_ini['pwd'], mysql_ini['host'],
+        mysql_ini['port'], mysql_ini['db'])
     app.config['SMS_WSDL_PARAMS'] = my_ini.get_webservice()
     del my_ini
     app.run(host=sys_ini['host'], port=sys_ini['port'], threaded=True)
