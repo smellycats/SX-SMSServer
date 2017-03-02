03c5b598c673241cf625132dac5769b3ba74d57d
SmellyCat <smellycat2014@foxmail.com>
2017-03-02 16:37:06 +0800

    更改 wsdl测试代码

--- wsdl_test.py
+++ wsdl_test.py
@@ -3,12 +3,13 @@ from sms.soap_func import SMSClient
 
 
 def sms_test():
-    url = 'http://10.44.237.88/axis/services/SMsg?wsdl'
+    url = 'http://10.44.237.123/axis/services/SMsg?wsdl'
     sms = SMSClient(url)
-    sms.sms_init('10.44.237.88', 'mas', '3306', 'kkxt', 'kkxt')
+    sms.sms_init('10.44.237.123', 'mas', '3306', 'kkxt', 'kkxt')
+    print help(sms)
     r = sms.sms_send('kkxt', 'kkxt', 'kkxt', ['15819851862', '13556222300'],
                      u'1234567', 2)
-    print type(r)
+    help(sms)
     del sms
 
 
