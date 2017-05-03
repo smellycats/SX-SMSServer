446fe82b3829c5245485a168f66bbf25a280bafe
SmellyCat <smellycat2014@foxmail.com>
2017-05-03 16:15:35 +0800

    修正 创建和修改时间本地化问题

--- sms/models.py
+++ sms/models.py
@@ -12,21 +12,23 @@ class Users(db.Model):
     username = db.Column(db.String(20), index=True)
     password = db.Column(db.String(255))
     scope = db.Column(db.String(255), default='')
-    date_created = db.Column(db.DateTime, default=arrow.now().datetime)
-    date_modified = db.Column(db.DateTime, default=arrow.now().datetime)
+    date_created = db.Column(
+        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
+    date_modified = db.Column(
+        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
     banned = db.Column(db.Integer, default=0)
 
-    def __init__(self, username, password, scope='', banned=0,
-                 date_created=None, date_modified=None):
+    def __init__(self, username, password, scope='', date_created=None,
+                 date_modified=None, banned=0):
         self.username = username
         self.password = password
         self.scope = scope
-        now = arrow.now().datetime
-        if not date_created:
+        now = arrow.now('PRC').datetime.replace(tzinfo=None)
+        if date_created is None:
             self.date_created = now
         else:
             self.date_created = date_created
-        if not date_modified:
+        if date_modified is None:
             self.date_modified = now
         else:
             self.date_modified = date_modified
@@ -55,23 +57,24 @@ class SMS(db.Model):
     __tablename__ = 'sms'
 
     id = db.Column(db.Integer, primary_key=True)
-    date_send = db.Column(db.DateTime, default=arrow.now().datetime)
+    date_send = db.Column(
+        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
     mobiles = db.Column(db.String(255), default='[]')
-    content = db.Column(db.Text)
+    content = db.Column(db.Text, default='')
     returned_value = db.Column(db.Integer, default=-99)
-    user_id = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, default=1)
 
     def __init__(self, date_send=None, mobiles='[]', content='',
                  returned_value=-99, user_id=0):
-        if not date_send:
-            self.date_send = arrow.now().datetime
+        if date_send is None:
+            self.date_send = arrow.now('PRC').datetime.replace(tzinfo=None)
         else:
             self.date_send = date_send
         self.mobiles = mobiles
         self.content = content
         self.returned_value = returned_value
         self.user_id = user_id
-
+        
     def __repr__(self):
         return '<SMS %r>' % self.id
 
@@ -84,8 +87,10 @@ class Phone(db.Model):
     user_id = db.Column(db.Integer, default=1)
     mobiles = db.Column(db.Text, default='[]')
     content = db.Column(db.Text, default='')
-    date_created = db.Column(db.DateTime)
-    date_modified = db.Column(db.DateTime)
+    date_created = db.Column(
+        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
+    date_modified = db.Column(
+        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
     banned = db.Column(db.Integer, default=0)
 
     def __init__(self, user_id=1, mobiles='[]', content='', date_created=None,
@@ -93,10 +98,17 @@ class Phone(db.Model):
         self.user_id = user_id
         self.mobiles = mobiles
         self.content = content
-        self.date_created = date_created
-        self.date_modified = date_modified
+        now = arrow.now('PRC').datetime.replace(tzinfo=None)
+        if date_created is None:
+            self.date_created = now
+        else:
+            self.date_created = date_created
+        if date_modified is None:
+            self.date_modified = now
+        else:
+            self.date_modified = date_modified
         self.banned = banned
-
+        
     def __repr__(self):
         return '<Phone %r>' % self.id
 
