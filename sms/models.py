e9c0ff26eca384293be202c2d2e9fca1470d9549
SmellyCat <smellycat2014@foxmail.com>
2017-09-25 17:03:18 +0800

    删除 无效的表

--- sms/models.py
+++ sms/models.py
@@ -9,47 +9,15 @@ class Users(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), index=True)
-    password = db.Column(db.String(255))
-    scope = db.Column(db.String(255), default='')
-    date_created = db.Column(
-        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
-    date_modified = db.Column(
-        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
+    username = db.Column(db.String(32), index=True)
     banned = db.Column(db.Integer, default=0)
 
-    def __init__(self, username, password, scope='', date_created=None,
-                 date_modified=None, banned=0):
+    def __init__(self, username, banned=0):
         self.username = username
-        self.password = password
-        self.scope = scope
-        now = arrow.now('PRC').datetime.replace(tzinfo=None)
-        if date_created is None:
-            self.date_created = now
-        else:
-            self.date_created = date_created
-        if date_modified is None:
-            self.date_modified = now
-        else:
-            self.date_modified = date_modified
         self.banned = banned
 
     def __repr__(self):
         return '<Users %r>' % self.id
-
-
-class Scope(db.Model):
-    """权限范围"""
-    __tablename__ = 'scope'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return '<Scope %r>' % self.id
 
 
 class SMS(db.Model):
@@ -63,9 +31,10 @@ class SMS(db.Model):
     content = db.Column(db.Text, default='')
     returned_value = db.Column(db.Integer, default=-99)
     user_id = db.Column(db.Integer, default=1)
+    user_info = db.Column(db.String(32), default='')
 
     def __init__(self, date_send=None, mobiles='[]', content='',
-                 returned_value=-99, user_id=0):
+                 returned_value=-99, user_id=0, user_info=''):
         if date_send is None:
             self.date_send = arrow.now('PRC').datetime.replace(tzinfo=None)
         else:
@@ -74,41 +43,9 @@ class SMS(db.Model):
         self.content = content
         self.returned_value = returned_value
         self.user_id = user_id
+        self.user_info = user_info
         
     def __repr__(self):
         return '<SMS %r>' % self.id
 
-
-class Phone(db.Model):
-    """短信记录"""
-    __tablename__ = 'phone'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, default=1)
-    mobiles = db.Column(db.Text, default='[]')
-    content = db.Column(db.Text, default='')
-    date_created = db.Column(
-        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
-    date_modified = db.Column(
-        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
-    banned = db.Column(db.Integer, default=0)
-
-    def __init__(self, user_id=1, mobiles='[]', content='', date_created=None,
-                 date_modified=None, banned=0):
-        self.user_id = user_id
-        self.mobiles = mobiles
-        self.content = content
-        now = arrow.now('PRC').datetime.replace(tzinfo=None)
-        if date_created is None:
-            self.date_created = now
-        else:
-            self.date_created = date_created
-        if date_modified is None:
-            self.date_modified = now
-        else:
-            self.date_modified = date_modified
-        self.banned = banned
-        
-    def __repr__(self):
-        return '<Phone %r>' % self.id
 
