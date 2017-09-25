b07f5cabb9e6e10e2a01bf217c64226a75e4d245
SmellyCat <smellycat2014@foxmail.com>
2017-09-25 17:07:49 +0800

    更改 使用mysql

--- sms/config.py
+++ sms/config.py
@@ -2,7 +2,7 @@
 
 class Config(object):
     # 密码 string
-    SECRET_KEY = 'hellokitty'
+    SECRET_KEY = 'thefatboy'
     # 服务器名称 string
     HEADER_SERVER = 'SX-SMSServer'
     # 加密次数 int
@@ -10,7 +10,7 @@ class Config(object):
     # token生存周期，默认2小时 int
     EXPIRES = 7200
     # 数据库连接 string
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../sms.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/sms'
     # 数据库连接 dict
     SQLALCHEMY_BINDS = {}
     # 连接池大小 int
